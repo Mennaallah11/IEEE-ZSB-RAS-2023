e9e50c45342f4688f4932389c07678d7a519d2b2
menna allah mohamed <120064762+Mennaallah11@users.noreply.github.com>
2023-07-24 17:30:45 +0300

    Delete LCD_DRIVER directory

--- LCD_DRIVER/LCD_DRIVER/5-LIBRARIES/BIT_MATH.h
+++
@@ -1,7 +0,0 @@
-#ifndef BIT_MATH_H
-#define BIT_MATH_H
-#define GET_BIT(VAR, BITNO)   ((VAR >> BITNO)& 1)
-#define SET_BIT(VAR, BITNO)   VAR |= (1 << BITNO)	
-#define CLR_BIT(VAR, BITNO)   VAR &= ~(1 << BITNO)
-#define TOG_BIT(VAR, BITNO)	  VAR ^= (1 << BITNO)
-#endif
