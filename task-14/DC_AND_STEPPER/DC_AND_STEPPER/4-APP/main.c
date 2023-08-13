035ba134908d8a3803af56d82a39776082a6e643
menna allah mohamed <120064762+Mennaallah11@users.noreply.github.com>
2023-08-13 21:14:43 +0300

    Update main.c

--- task-14/DC_AND_STEPPER/DC_AND_STEPPER/4-APP/main.c
+++ task-14/DC_AND_STEPPER/DC_AND_STEPPER/4-APP/main.c
@@ -21,242 +21,188 @@
 
 int main(void)
 {
-   
-  /*  DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN0,DIO_u8_OUTPUT);
-	DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN1,DIO_u8_OUTPUT);
-	DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN2,DIO_u8_OUTPUT);
-	DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN3,DIO_u8_OUTPUT); 
-	                                              
-    DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN4,DIO_u8_INPUT);
-	DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN5,DIO_u8_INPUT);
-	DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN6,DIO_u8_INPUT);
-	DIO_u8SetPinDirection(DIO_u8_PORTA,DIO_u8_PIN7,DIO_u8_INPUT);
-	                                                      	
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN0,DIO_u8_HIGH);
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN1,DIO_u8_HIGH);
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN2,DIO_u8_HIGH);
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN3,DIO_u8_HIGH);
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN4,DIO_u8_HIGH);
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN5,DIO_u8_HIGH);
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN6,DIO_u8_HIGH);
-	DIO_u8SetPinValue(DIO_u8_PORTA,DIO_u8_PIN7,DIO_u8_HIGH); */
-	
-	
 	KPD_enuInit();
 	LCD_enuInit();
-	//DcMotor_voidInit();
-	//stepper_enuInit();
-	u8 ReturnedKey  ;
-	
+	u8 var;
 	u8 ThePassword[PasswordLength] = the_Password;
-	u8 Password[PasswordLength]={0};
-	u8 index ;
-	u8 flag = 0 ; 
-	u8 check = 0 ;
+	u8 Password[PasswordLength] = { 0 };
+	u8 index;
+	u8 flag = 0;
+	u8 check = 0;
 	u8 option;
-	while(1)
-{
-	/*LCD_enuSetAc(1,5);
-     LCD_enuSendString("Enter_Password:");
-	_delay_ms(100);
-	LCD_enuClear();*/
+	LCD_enuSetAc(LCD_u8_LINE1, 0);
+	LCD_enuSendString("Password: ");
 
-    KPD_enuGetKeyState(&ReturnedKey);
-	if (ReturnedKey != KPD_u8_KEY_NOT_PRESSED )
-	{
-		LCD_enuSendChar('*');
-		Password[index] = ReturnedKey ;
-		index ++ ; 
-		check ++ ;
-	}
-/	if ((index)== PasswordLength)
-	{
-		for (int i = 0 ; i < PasswordLength ; i ++ )
+	while (1)
 		{
-			if (Password[i]!=ThePassword[i])
-			{
-				 flag=1;
-				 break;	       
+		KPD_enuGetKeyState(&var);
+		if (var != 0xff) {
+			LCD_enuSendChar('*');
+			Password[index] = var;
+			index++;
+		}
+		if ((index) == PasswordLength) {
+			flag = 1;
+			for (int i = 0; i < PasswordLength; i++) {
+				if (Password[i] != ThePassword[i]) {
+					flag = 0;
+				}
 			}
-		 }
-		if(flag)
-		{
-			LCD_enuClear();
-			LCD_enuSendString("Wrong Password");
-			 _delay_ms(1);
-			LCD_enuClear();
-			check = ++ ;
-			index = 0 ;
-			flag = 0 ;
-			if (check >= max_try) 
-			{
+			if (flag) {
 				LCD_enuClear();
-				LCD_enuSendString("Error :( ");
-				_delay_ms(100);
+				LCD_enuSendString("True Password :)");
+				delay();
 				LCD_enuClear();
-				check = 0 ;
-		
+				LCD_enuSendString("choose_motor");
+				delay();
+				LCD_enuClear();
+				LCD_enuSetAc(LCD_u8_LINE1, 0);
+				LCD_enuSendString("1-DC ");
+				LCD_enuSendString("2-stepper");
+				LCD_enuSetAc(LCD_u8_LINE2, 0);
+				LCD_enuSendString("3-EXIT");
+//				LCD_enuClear();
+				while (1) {
+					KPD_enuGetKeyState(&option);
+					if (option != KPD_u8_KEY_NOT_PRESSED) {
+						if (option == '1') {
+							DC_direction();
+						} else if (option == '2') {
+							get_angle();
+							stepper_direction();
+						}
+
+						else if (option == '3') {
+							LCD_enuSendString(" thank you ");
+
+						}
+					}
+				}
+
+				break;
+			} else {
+				LCD_enuClear();
+				LCD_enuSetAc(1, 0);
+				LCD_enuSendString("Wrong Password :)");
+				delay();
+				LCD_enuClear();
+				check++;
+				index = 0;
+				if (check >= max_try) {
+					LCD_enuSetAc(1, 0);
+					LCD_enuSendString("Error :( ");
+					delay();
+					LCD_enuClear();
+					//	break;
+					check = 0;
+				}
 			}
 		}
-		else 
-		{
-			LCD_enuClear();
-			LCD_enuSendString("True Password :)");
-			  _delay_ms(100);
-			LCD_enuClear();
-			LCD_enuSendString("choose_motor");
-			
-			_delay_ms(100);
-			LCD_enuClear();
-			LCD_enuSetAc(LCD_u8_LINE1,0);
-			LCD_enuSendString("1-DC");
-		
-			LCD_enuSetAc(LCD_u8_LINE1,5);
-			LCD_enuSendString("2-stepper");
-			
-			LCD_enuSetAc(LCD_u8_LINE2,0);
-			LCD_enuSendString("3-EXIT");
-		
-			
-		
-		
-			KPD_enuGetKeyState(&option);
-			if (option != KPD_u8_KEY_NOT_PRESSED )
-			{
-				if (option == '1')
-				{
-					DC_direction();
-				}
-				else if (option =='2')
-			    {
-					get_angle();
-					stepper_direction();		
-				}
-
-				else if (option =='3')	
-				{
-					LCD_enuSendString(" thank you ");
-				
-				}
-			}
-			break;
-        }
 	}
-}
-   
 	return 0;
 }
 
-void stepper_direction()
-{
-	 u8 direction ;
-	 u8 angle =0 ;
-	
-	 LCD_enuClear();
-	 LCD_enuSetAc(LCD_u8_LINE1,0);
-	 LCD_enuSendString("1_CW");
+void stepper_direction() {
+	u8 direction;
+	u8 angle = 0;
 
-	 LCD_enuSetAc(LCD_u8_LINE1,5);
-	 LCD_enuSendString("2_ACW");
-		LCD_enuSendChar('A');
-			
+	LCD_enuClear();
+	LCD_enuSetAc(LCD_u8_LINE1, 0);
+	LCD_enuSendString("1_CW");
 
-		 	 KPD_enuGetKeyState(&direction);
-			 if (direction != KPD_u8_KEY_NOT_PRESSED )
-			 {
-				 if (direction== '1') 
-				 {
-					 LCD_enuClear();
-					 stepper_enuInit();
-					 stepper_enuHalfStep(CW, angle);
-					// break;
-				 }
-				 else if (direction== '2')
-				 {
-					 LCD_enuClear();
-					 stepper_enuInit();
-					 stepper_enuHalfStep(ACW, angle);
-					// break;
-				 }
-			 }
+	LCD_enuSetAc(LCD_u8_LINE1, 5);
+	LCD_enuSendString("2_ACW");
+	delay();
+//	LCD_enuClear();
+
+	while (1) {
+
+		KPD_enuGetKeyState(&direction);
+		if (direction != KPD_u8_KEY_NOT_PRESSED) {
+			if (direction == '1') {
+				LCD_enuClear();
+				stepper_enuInit();
+				stepper_enuHalfStep(CW, angle);
+				break;
+			} else if (direction == '2') {
+				LCD_enuClear();
+				stepper_enuInit();
+				stepper_enuHalfStep(ACW, angle);
+				break;
+			}
+		}
+	}
 }
 
-void DC_direction(void)
-{
-	 u8 direction ;
-    LCD_enuClear();
-	LCD_enuSetAc(LCD_u8_LINE1,0);
+void DC_direction(void) {
+	u8 direction;
+	LCD_enuClear();
+	LCD_enuSetAc(LCD_u8_LINE1, 0);
 	LCD_enuSendString("1_CW");
-	 
-	LCD_enuSetAc(LCD_u8_LINE1,5);
-    LCD_enuSendString("2_ACW");
-	 
-	
-		 KPD_enuGetKeyState(&direction);
-		 if (direction != KPD_u8_KEY_NOT_PRESSED )
-		 {
-			if (direction== '1') 
-			{
-				LCD_enuClear();
+
+	LCD_enuSetAc(LCD_u8_LINE1, 5);
+	LCD_enuSendString("2_ACW");
+
+	delay();
+//	LCD_enuClear();
+	while (1) {
+		KPD_enuGetKeyState(&direction);
+		if (direction != 0xff) {
+			if (direction == '1') {
+//				LCD_enuClear();
 				DcMotor_voidInit();
 				DcMotor_enuStart(CW);
-			//	break;
-			}
-			else if (direction== '2')
-			{
-				LCD_enuClear();
+				break;
+			} else if (direction == '2') {
+//				LCD_enuClear();
 				DcMotor_voidInit();
 				DcMotor_enuStart(ACW);
-			//	break;
+				break;
 			}
-		 }
+		}
+	}
 }
 
 
-void get_angle()
-{
-    u8 degree ;
-	u8 angle ;
-	
-	     LCD_enuClear();
-		 LCD_enuSetAc(LCD_u8_LINE1,0);
-		 LCD_enuSendString("1_90");
-	
-		 LCD_enuSetAc(LCD_u8_LINE1,5);
-		 LCD_enuSendString("2_180");
-		 
-		 LCD_enuSetAc(LCD_u8_LINE2,0);
-		 LCD_enuSendString("3-270");
-	    
-		 LCD_enuClear(); 
-		 
-		 KPD_enuGetKeyState(&degree);
-		 if (degree != KPD_u8_KEY_NOT_PRESSED )
-		 {
-			 if (degree =='1')
-			 {
-				 angle = 90;
-				 stepper_direction();
-				// break ;
-				 
-			 }
-			 else if (degree =='2')
-			 {
-				 angle = 180;
-				 stepper_direction();
-				// break ;
-				 
-			 }
-			 else if (degree =='3')
-			{
-				 angle = 270;
-				 stepper_direction();
-				// break ;
+
+
+void get_angle() {
+	u8 degree;
+	u8 angle;
+
+	LCD_enuClear();
+	LCD_enuSetAc(LCD_u8_LINE1, 0);
+	LCD_enuSendString("1_90");
+	LCD_enuSetAc(LCD_u8_LINE1, 5);
+	LCD_enuSendString("2_180");
+	LCD_enuSetAc(LCD_u8_LINE2, 0);
+	LCD_enuSendString("3-270");
+	LCD_enuSendChar('7');
+
+//	LCD_enuClear();
+	while (1) {
+
+		KPD_enuGetKeyState(&degree);
+		if (degree != KPD_u8_KEY_NOT_PRESSED) {
+			if (degree == '1') {
+				angle = 90;
+				stepper_direction();
+				break;
+
+			} else if (degree == '2') {
+				angle = 180;
+				stepper_direction();
+				break;
+
+			} else if (degree == '3') {
+				angle = 270;
+				stepper_direction();
+				break;
 			}
-		
-		 }
-}
-		 
+
+		}
+	}
+}	 
 		 
 		 
 
